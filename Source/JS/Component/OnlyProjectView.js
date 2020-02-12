ea7ade70b26eea9cc515ee7bbba351051cc76749
lian0123 <lienyungli123@gmail.com>
2020-02-13 02:04:10 +0800

    除了單一作品檢視頁面只完成layuot與基本控制外 其他PC版上的界面已完成

--- Source/JS/Component/OnlyProjectView.js
+++ Source/JS/Component/OnlyProjectView.js
@@ -1,6 +1,137 @@
 var OnlyProjectView = {
     template: 
     `
-        <div>OnlyProjectView</div>
+        <div>
+            <!--載入背景-->
+            <div id="MainBackground"></div>
+
+            <!--載入返回鍵-->
+            <div id="UpBtn">
+                <a @click="ChangeBeforePage()">
+                    <img height="2.5%" width="2.5%" src="./Source/IMG/UpBTN.svg" />
+                </a>
+            </div>
+        
+            <div id="ProjectMetaBlock">
+                <div class="container">
+                    <p>{{ProjectMeta}}</p>
+                </div>
+            </div>
+
+            <div id="ProjectMainBlock">
+                <div class="container">
+                    <p>{{ProjectPhotoPath}}</p>
+                    <a onclick="StudentGroups.event.GetBeforeGroup()">上一組</a>
+                    <a onclick="StudentGroups.event.GetNextGroup()">下一組</a>
+                </div>
+            </div>
+
+            <div id="ImagePhotoBlock">
+                <div class="container">
+                    <p>{{ImagePhotoPath}}</p>
+                </div>
+            </div>
+
+            <div id="AboutBlock" class="container">
+                <div class="container">
+                    <p>{{About}}</p>
+                </div>
+            </div>
+        </div>
     `,
+    data() {
+        return {
+            IsProjectAboutView  : false                           , //是否檢視作品說明界面
+            IsProjectMumberView : false                           , //是否檢視參與成員資料界面
+            Link                : StudentGroups.data.FaceBookLink , //載入FB連結
+            NowSelectGroupID    : 0                               , //目前選擇的作品ID(Array索引值)
+        }
+    },
+    async mounted() {
+        try{
+            /* 用同位異步方式與await等待app元件優先載入完成，並讀取應該讀取的索引值 */
+            this.NowSelectGroupID = await(app.NowSelectID);
+        } catch (error) {
+            /* 當路由直接指向此頁時，無法獲取有效的ID值定給予初始值索引編號0 */
+            this.NowSelectGroupID = 0;
+        };
+        
+        /* 執行載入顯示元件動畫 */
+        document.getElementsByTagName("div").UpBtn.className            = "animated fadeInDown   delay-1s";
+        document.getElementsByTagName("div").ProjectMetaBlock.className = "animated slideInLeft  delay-1s";
+        document.getElementsByTagName("div").ProjectMainBlock.className = "animated fadeInUp     delay-1s";
+        document.getElementsByTagName("div").ImagePhotoBlock.className  = "animated fadeInDown   delay-1s";
+        document.getElementsByTagName("div").AboutBlock.className       = "animated slideInRight delay-1s";
+
+        setInterval(() => {
+            this.NowSelectGroupID = StudentGroups.event.GetNowSelectID();
+        },500);
+    },
+    methods: {
+        /* 切換到上一頁(手機版作品專欄) */
+        ChangeBeforePage:function() {
+            /* 執行收起元件動畫 */
+            document.getElementsByTagName("div").UpBtn.className            = "animated fadeOutUp     delay-1s";
+            document.getElementsByTagName("div").ProjectMetaBlock.className = "animated slideOutLeft  delay-1s";
+            document.getElementsByTagName("div").ProjectMainBlock.className = "animated slideOutDown  delay-1s";
+            document.getElementsByTagName("div").ImagePhotoBlock.className  = "animated fadeOutUp     delay-1s";
+            document.getElementsByTagName("div").AboutBlock.className       = "animated slideOutRight delay-1s";
+
+            app.DelayRouteBack(3000);
+        },
+        /* 設定是否要檢視作品說明界面 */
+        SetProjectAboutView:function(NextToShow) {
+            /* 載入偽浮動視窗移出的動畫(預設以綁定移入) */
+            if(!NextToShow){
+                document.getElementsByTagName("div").ProjectAboutView.className = "animated fadeOutDown";
+            }
+
+            /* 等待動畫跑完後執行關閉視窗檢視 */
+            setTimeout(() => {
+                this.IsProjectAboutView = NextToShow;
+            },500);
+        },
+        /* 設定是否要檢視參與成員資料界面 */
+        SetProjectMumberView:function(NextToShow) {
+            /* 載入偽浮動視窗移出的動畫(預設以綁定移入) */
+            if(!NextToShow){
+                document.getElementsByTagName("div").ProjectMumberView.className = "animated fadeOutDown";
+            }
+        
+            /* 等待動畫跑完後執行關閉視窗檢視 */
+            setTimeout(() => {
+                this.IsProjectMumberView = NextToShow;
+            },500);
+        },
+    },
+    computed: {
+        /* 載入形象照路徑 */
+        ImagePhotoPath:function() {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].ImagePhotoPath;
+        },
+        /*作品名稱 */
+        Name:function(value) {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].Name;
+        },
+        /*作品種類 */
+        Type:function(value) {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].Type;
+        },
+        /* 載入自介 */
+        About:function(value) {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].About;
+        },
+        /* 載入作品介紹 */
+        ProjectMeta:function(value) {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].ProjectMeta;
+        },
+        /* 載入作品影片路徑 */
+        ProjectMoviePath:function(value) {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].ProjectMoviePath;
+        },
+        /* 載入作品照片路徑 */
+        ProjectPhotoPath:function(value) {
+            return StudentGroups.data.GroupsData[this.NowSelectGroupID].ProjectPhotoPath;
+        }
+    },
 };
--- Source/JS/Component/ProjectTypeView.js
+++ Source/JS/Component/ProjectTypeView.js
@@ -13,14 +13,12 @@ var ProjectTypeView = {
             </div>
 
             <div id="ProgectTypeListGroup" class="container-fluid">
-                <div v-for="(item,index) in GroupsDataList" v-if="item.Type === $route.params.ProjectType">
-                    <div class="row">
-                        <div class="col">
-                            <div id="ThreeDimensionalModelingBTN">
-                                <a @click="ChangeNextPage(index)">
-                                <img height="100%" width="100%" src="./Source/IMG/PhoneMenuViewFacebookIcon.svg" />
-                                </a>
-                            </div>
+                <div v-for="(item,index) in GroupsDataList" class="row">
+                    <div v-for="(subitem,subindex) in item" class="col">
+                        <div id="ThreeDimensionalModelingBTN">
+                            <a @click="ChangeNextPage(index)">
+                                <img height="75%" width="75%" src="./Source/IMG/PhoneMenuViewFacebookIcon.svg" />
+                            </a>
                         </div>
                     </div>
 
@@ -39,6 +37,9 @@ var ProjectTypeView = {
         }
     },
     async mounted() {
+        let TmpArray = [];
+        let TmpCounter = 0;
+
         try {
             /* 用同位異步方式與await等待app元件優先載入完成，並讀取此頁面是否發生二次載入(選擇動畫方向用) */
             this.IsReMount = await(app.ProjectTypeViewIsGoBack);
@@ -50,31 +51,53 @@ var ProjectTypeView = {
         //document.getElementsByTagName("body")[0].style.position = "relative";
 
         /* 綁定StudentGroups下的GroupsData List */
-        this.GroupsDataList = StudentGroups.data.GroupsData;
+        this.GroupsDataList = [];
+        for (let i = 0; i < StudentGroups.data.GroupsData.length; i++) {
+            if(this.$route.params.ProjectType === StudentGroups.data.GroupsData[i].Type){
+        
+                TmpArray.push(StudentGroups.data.GroupsData[i]);
+
+                if((TmpCounter+1)%3 == 0){
+                    this.GroupsDataList.push(TmpArray);
+                    TmpArray   = [];
+                    TmpCounter = 0;
+                }else{
+                    TmpCounter++;
+                }
+            }
+            
+        }
+
+        if(TmpArray != []){
+            this.GroupsDataList.push(TmpArray);
+        }
+        
+        console.log(this.GroupsDataList);
+        
 
         /* 依據是否以載入，而作品形象照由對應方向載入 */
         if(!this.IsReMount){
-            document.getElementsByTagName("div").ProgectTypeListGroup.className           = "animated fadeInRight delay-0s";
+            document.getElementsByTagName("div").ProgectTypeListGroup.className           = "animated fadeInUp delay-0s";
         }else{
-            document.getElementsByTagName("div").ProgectTypeListGroup.className           = "animated fadeInLeft  delay-0s";
+            document.getElementsByTagName("div").ProgectTypeListGroup.className           = "animated fadeInDown  delay-0s";
         }
     },
     methods: {
         
-        /* 切換到上一頁(手機版作品一覽) */
+        /* 切換到上一頁(作品一覽) */
         ChangeBeforePage:function() {
-            document.getElementsByTagName("div").UpBtn.className           = "animated fadeOutUp delay-0s";
-            document.getElementsByTagName("body")[0].style.position = "Fixed";
-            app.DelayRouteBack(1000);
+            document.getElementsByTagName("div").UpBtn.className                    = "animated fadeOutUp delay-0s";
+            document.getElementsByTagName("div").ProgectTypeListGroup.className     = "animated fadeOutDown delay-1s";
+            app.DelayRoutePush("/Project",2000);
         },
-        /* 切換到下一頁(手機版小組作品) */
+        /* 切換到下一頁(小組作品) */
         ChangeNextPage:function(ProjectID) {
-            document.getElementsByTagName("div").UpBtn.className           = "animated fadeOutUp  delay-0s";
-            document.getElementsByTagName("body")[0].style.position = "Fixed";
+            document.getElementsByTagName("div").UpBtn.className                    = "animated fadeOutUp  delay-0s";
+            document.getElementsByTagName("div").ProgectTypeListGroup.className     = "animated fadeOutUp delay-1s";
             
             StudentGroups.event.SetNowSelectID(ProjectID);
             app.NowSelectID = ProjectID;
-            app.DelayRoutePush("/phoneProjectView",1000);
+            app.DelayRoutePush("/phoneProjectView",2000);
         }
     },
 };
